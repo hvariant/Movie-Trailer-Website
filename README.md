bd82400b2a72a161fa3886372b9348dd067f00bd
Jason Li <lizhansong@hotmail.com>
2017-03-03 20:44:59 +1100

    3 more movies, read me and comments

---
+++ README.md
@@ -0,0 +1,22 @@
+# Movie-Trailer-Website
+
+This is the repository for the first project of Udacity's Fullstack Web Developer
+[nanodegree](https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004)
+program. It generates an html file dynamically in Python from a list of movies. The
+html file shows the poster, title of the movies and clicking on the posters will open
+the movie's trailer.
+
+## Usage
+
+1.  To add or change the movie list edit the file 'movies.txt' and add or remove lines.
+    Each line should consist of the url of the movie's trailer URL, title, and poster URL
+    separated by space. If a movie title contains multiple words they should be joined by
+    the underscore character '\_'.
+
+2.  To generate and open the webpage run:
+
+        `./entertainment_center.py`
+   
+    This will automatically generate and open in a browser an html file called
+    `fresh_tomatoes.html`.
+ 
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,20 +1,18 @@
 #!/bin/env python2
 
-def parse_data(file_name):
-    import media
+import fresh_tomatoes
+import media
+
+def parse_data(file_name): # read and parse movie list from file
     movies = []
     with open(file_name,"r") as f:
         for line in f:
-            trailer_url, title, poster_url = line.strip().split()
-            title = title.replace("_"," ")
+            trailer_url, title, poster_url = line.strip().split() # separated by whitespace
+            title = title.replace("_"," ") # replace '_' with ' ' in movie titles because using ' ' in file confuses str.split()
             movies.append(media.Movie(trailer_url,title, poster_url))
 
     return movies
 
-def generate_website(movies):
-    import fresh_tomatoes
-    fresh_tomatoes.open_movies_page(movies)
-
 if __name__ == "__main__":
-    movies = parse_data("movies.txt")
-    generate_website(movies)
+    movies = parse_data("movies.txt") # hardcoded file name ¯\_(ツ)_/¯
+    fresh_tomatoes.open_movies_page(movies) # dynamically generate html file using list of movies
