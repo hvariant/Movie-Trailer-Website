fe4c904158e99631434ad978dbc32b1c16dd6efb
Jason Li <lizhansong@hotmail.com>
2017-03-05 17:38:02 +1100

    added docstring to Movie class

--- media.py
+++ media.py
@@ -1,6 +1,14 @@
 
 
 class Movie(object):
+    """Movie class that stores relevant information for representing a movie on a webpage.
+
+    Attributes:
+        trailer_youtube_url (str): youtube URL of movie trailer
+        title (str): title of movie
+        poster_image_url (str): URL of movie poster
+    """
+
     def __init__(self, trailer_url, title, poster_url):
         self.trailer_youtube_url = trailer_url
         self.title = title
